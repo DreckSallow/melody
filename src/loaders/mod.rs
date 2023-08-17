b84be3ca9f10fad644b89dc4d6d873c32ccfce22
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-08-17 16:23:34 -0500

    split the logic of the toml data

--- src/loaders/mod.rs
+++ src/loaders/mod.rs
@@ -1,52 +1,18 @@
-use anyhow::{anyhow, Ok, Result};
-use serde::Deserialize;
-use std::{fs, path::PathBuf};
+use anyhow::{anyhow, Result};
+use std::fs;
 
-use directories::ProjectDirs;
+use crate::dirs::playlists_path;
 
-pub fn local_data_dir() -> Option<PathBuf> {
-    if let Some(proj_dirs) = ProjectDirs::from("com", "Oxide", "Melody") {
-        let local_data_dir = proj_dirs.data_local_dir();
-        if !local_data_dir.exists() {
-            if let Err(_) = fs::create_dir_all(local_data_dir) {
-                return None;
-            }
-        }
-        return Some(local_data_dir.into());
-    }
-    return None;
-}
-pub fn playlists_path() -> Option<PathBuf> {
-    if let Some(mut local_data_path) = local_data_dir() {
-        local_data_path.push("data.toml");
-        if !local_data_path.exists() {
-            if let Err(_) = fs::File::create(&local_data_path) {
-                return None;
-            }
-        }
-        Some(local_data_path)
-    } else {
-        None
-    }
-}
+use self::player::{PlaylistsData, RawToml};
 
-#[derive(Clone, Debug, Deserialize)]
-pub struct PlaylistsData {
-    pub playlists: Vec<PlaylistInfo>,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct PlaylistInfo {
-    pub name: String,
-    pub songs: Vec<String>,
-}
+pub mod player;
 
 pub fn load_playlists() -> Result<PlaylistsData> {
     match playlists_path() {
         Some(path) => {
             let data = fs::read_to_string(path)?;
-            let datin: PlaylistsData = toml::from_str(&data)?;
-            Ok(datin)
+            let datin: RawToml = toml::from_str(&data)?;
+            PlaylistsData::try_from(datin)
         }
         None => Err(anyhow!("Not was posible get the data info")),
     }
