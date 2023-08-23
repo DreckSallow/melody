25a93c2e042b77999aae0c1b3c1b6617351c3eaf
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-08-23 15:29:28 -0500

    change to mutable state in component render method

--- src/component.rs
+++ src/component.rs
@@ -11,7 +11,7 @@ pub(crate) type FrameType<'a> = Frame<'a, CrosstermBackend<Stdout>>;
 
 pub trait Component {
     type State;
-    fn render(&mut self, frame: &mut FrameType, area: Rect, _state: &Self::State);
+    fn render(&mut self, frame: &mut FrameType, area: Rect, _state: &mut Self::State);
     fn on_event(&mut self, event: &AppEvent, _state: &mut Self::State);
     fn is_focus(&self) -> bool {
         false
