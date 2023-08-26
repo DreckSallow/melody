4300ea86d27a97112400496ca31f384b28b7c5f8
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-08-25 21:39:52 -0500

    create the audio folder

--- src/tabs/player/audio/mod.rs
+++ src/tabs/player/audio/mod.rs
@@ -1,10 +1,4 @@
-use std::{
-    fs::File,
-    io::BufReader,
-    rc::Rc,
-    time::{Duration, Instant},
-};
-
+use crate::utils::Condition;
 use anyhow::Result;
 use crossterm::event::{KeyCode, KeyEventKind};
 use ratatui::{
@@ -12,149 +6,21 @@ use ratatui::{
     style::{Color, Style},
     widgets::{block::Title, Block, Borders, Gauge, Paragraph},
 };
-use rodio::{Decoder, OutputStream, Sink, Source};
+use std::rc::Rc;
 
 use crate::{
     app::AppState,
     component::{Component, FrameType},
     event::AppEvent,
     loaders::player::PlaylistSong,
+    select,
     tabs::log::{LogMessage, LogsState},
 };
 
 use super::state::PlayerState;
 
-#[derive(Debug)]
-struct Progress {
-    duration: Duration,
-    timer: Option<Instant>,
-}
-
-impl Default for Progress {
-    fn default() -> Self {
-        Self::new(Duration::default(), None)
-    }
-}
-
-impl Progress {
-    pub fn new(duration: Duration, timer: Option<Instant>) -> Self {
-        Self { duration, timer }
-    }
-    pub fn pause(&mut self) {
-        if let Some(timer) = self.timer.as_ref() {
-            self.duration += timer.elapsed();
-            self.timer = None;
-        }
-    }
-    pub fn start(&mut self) {
-        self.timer = Some(Instant::now())
-    }
-    pub fn seconds(&self) -> u64 {
-        match self.timer {
-            Some(ref timer) => (self.duration + timer.elapsed()).as_secs(),
-            None => self.duration.as_secs(),
-        }
-    }
-    pub fn percentage(&self, other: Duration) -> u8 {
-        let percentage = (self.seconds() * 100) / other.as_secs();
-        if percentage >= 100 {
-            100
-        } else {
-            percentage as u8
-        }
-    }
-}
-
-enum AudioStatus {
-    Pause,
-    Play,
-}
-
-struct AudioHandler {
-    song: Option<PlaylistSong>,
-    sink: Sink,
-    _stream: OutputStream,
-    status: AudioStatus,
-    progress: Progress,
-}
-
-impl AudioHandler {
-    pub fn try_default() -> Result<Self> {
-        let (_stream, handle) = OutputStream::try_default()?;
-        let sink = Sink::try_new(&handle)?;
-        sink.pause();
-
-        Ok(Self {
-            sink,
-            _stream,
-            song: None,
-            status: AudioStatus::Pause,
-            progress: Progress::default(),
-        })
-    }
-
-    pub fn pause(&mut self) {
-        self.sink.pause();
-        self.progress.pause();
-        self.status = AudioStatus::Pause;
-    }
-    pub fn play(&mut self) {
-        self.sink.play();
-        self.progress.start();
-        self.status = AudioStatus::Play;
-    }
-    pub fn finish(&mut self) {
-        self.sink.pause();
-        self.sink.stop();
-    }
-    pub fn percentage_info(&mut self, other: Duration) -> (u64, u8) {
-        let info = (self.progress.seconds(), self.progress.percentage(other));
-        if info.1 >= 100 {
-            self.pause();
-        }
-
-        info
-    }
-    pub fn is_end_song(&self) -> bool {
-        if let Some(ref song) = self.song {
-            let p = self.progress.percentage(song.duration);
-            if p >= 100 {
-                true
-            } else {
-                false
-            }
-        } else {
-            true
-        }
-    }
-
-    pub fn set_song(&mut self, song_opt: Option<PlaylistSong>) -> Result<()> {
-        if let Some(song) = &song_opt {
-            let file_song = BufReader::new(File::open(&song.path)?);
-            let decoder = Decoder::new(file_song)?;
-            self.append(decoder);
-        }
-        self.song = song_opt;
-        Ok(())
-    }
-    pub fn append(&mut self, decoder: Decoder<BufReader<File>>) {
-        if !self.sink.empty() {
-            self.sink.stop();
-        };
-        self.sink.append(decoder);
-        self.progress = Progress::default();
-
-        if let AudioStatus::Play = self.status {
-            self.play();
-        }
-    }
-    pub fn toggle_action(&mut self) {
-        match self.status {
-            AudioStatus::Pause => self.play(),
-            AudioStatus::Play => self.pause(),
-        }
-    }
-}
+mod handler;
+use handler::AudioHandler;
 
 pub struct AudioPlayer {
     handler: AudioHandler,
@@ -174,9 +40,6 @@ impl AudioPlayer {
         if let Err(e) = handler.set_song(song) {
             logger.borrow_mut().push(LogMessage::Error(e.to_string()))
         }
-        logger
-            .borrow_mut()
-            .push(LogMessage::Info("SET SONG".into()));
 
         Ok(Self {
             handler,
@@ -226,18 +89,13 @@ impl Component for AudioPlayer {
         state: &mut Self::State,
     ) {
         self.on_tick(state);
-        let styled = if self.is_focus {
-            Style::default().fg(Color::Cyan)
-        } else {
+        let styled = select!(
+            self.is_focus,
+            Style::default().fg(Color::Cyan),
             Style::default()
-        };
+        );
 
-        let block_title = self
-            .handler
-            .song
-            .as_ref()
-            .and(Some("Now Playing"))
-            .unwrap_or("Not song");
+        let block_title = select!(self.handler.song(), "Now Playing", "Not Song");
 
         let block = Block::default()
             .title(Title::from(block_title).alignment(Alignment::Center))
@@ -245,7 +103,7 @@ impl Component for AudioPlayer {
             .style(styled);
         frame.render_widget(block, area);
 
-        match self.handler.song.clone() {
+        match self.handler.song().cloned() {
             Some(song) => {
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
