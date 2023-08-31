54c862852b385cb2bef40a4033071bd2061263de
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-08-30 22:32:17 -0500

    create the data folder to contain utils for get local info

---
+++ src/data/playlists.rs
@@ -0,0 +1,44 @@
+use std::{fs, path::PathBuf};
+
+use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
+
+use crate::dirs::local_data_dir;
+
+#[derive(Deserialize, Serialize)]
+pub struct RawPlaylistToml {
+    pub playlists: Vec<RawPlaylist>,
+}
+#[derive(Deserialize, Serialize)]
+pub struct RawPlaylist {
+    pub name: String,
+    pub songs: Vec<String>,
+}
+pub struct PlaylistStore;
+
+impl PlaylistStore {
+    pub const FILE: &str = "data.toml";
+    pub fn path() -> Result<PathBuf> {
+        match local_data_dir() {
+            Some(mut p) => {
+                p.push(Self::FILE);
+                if !p.exists() {
+                    fs::File::create(&p)?;
+                }
+                Ok(p)
+            }
+            None => Err(anyhow!("Not was posible get the config")),
+        }
+    }
+    pub fn load() -> Result<RawPlaylistToml> {
+        let p = Self::path()?;
+        let data: RawPlaylistToml = toml::from_str(&fs::read_to_string(p)?)?;
+        Ok(data)
+    }
+    pub fn save(data: RawPlaylistToml) -> Result<()> {
+        let p = Self::path()?;
+        let data_toml = toml::to_string(&data)?;
+        fs::write(p, data_toml)?;
+        Ok(())
+    }
+}
